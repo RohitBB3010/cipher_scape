f7576fbbaf70a7f55030c5585d3402390f13cd1c
Rohit Bharat Bhandwalkar <rohitb.bhandwalkar@gmail.com>
2024-06-06 01:25:46 +0530

    Added padding to mandatory fields page

--- lib/screens/mandatory_fields/mandatory_fields_page.dart
+++ lib/screens/mandatory_fields/mandatory_fields_page.dart
@@ -4,6 +4,7 @@ import 'package:cipher_affair/components/custom_button.dart';
 import 'package:cipher_affair/components/loading_page.dart';
 import 'package:cipher_affair/consts/colors.dart';
 import 'package:cipher_affair/consts/spacing_consts.dart';
+import 'package:cipher_affair/screens/auth/auth_cubit.dart';
 import 'package:cipher_affair/screens/home/home_page.dart';
 import 'package:cipher_affair/screens/mandatory_fields/mandatory_field_state.dart';
 import 'package:cipher_affair/screens/mandatory_fields/mandatory_fields_cubit.dart';
@@ -29,78 +30,98 @@ class MandatoryFieldsPage extends StatelessWidget {
           if (state is MandatoryFieldsNotFilledState) {
             return Scaffold(
               backgroundColor: primary_3,
-              body: SizedBox(
-                width: MediaQuery.of(context).size.width,
-                child: SingleChildScrollView(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      SpacingConsts().largeHeightBetweenFields(context),
-                      const AutoSizeText(
-                        'Cipher Scape',
-                        style: TextStyle(
-                            fontFamily: 'Legio',
-                            fontSize: 40.0,
-                            color: Colors.amber),
-                      ),
-                      SpacingConsts().mediumHeightBetweenFields(context),
-                      const AutoSizeText(
-                        'Join the ranks!! Identify yourself!',
-                        style: TextStyle(
-                            fontFamily: 'Kod',
-                            fontSize: 20.0,
-                            color: Colors.amber),
-                      ),
-                      SpacingConsts().mediumHeightBetweenFields(context),
-                      SizedBox(
-                        width: MediaQuery.of(context).size.width * 0.7,
-                        child: CustomTextField(
-                          hintText: 'Enter name',
-                          controller: nameController,
-                          prefixIcon: Icons.person,
+              body: Padding(
+                padding: EdgeInsets.symmetric(
+                    horizontal: MediaQuery.of(context).size.width * 0.05),
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  child: SingleChildScrollView(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        SpacingConsts().largeHeightBetweenFields(context),
+                        SizedBox(
+                            width: MediaQuery.of(context).size.width * 0.3,
+                            height: MediaQuery.of(context).size.height * 0.2,
+                            child: Image.asset('assets/logo_new.jpeg')),
+                        const AutoSizeText(
+                          'Cipher Scape',
+                          maxLines: 1,
+                          style: TextStyle(
+                              fontFamily: 'Legio',
+                              fontSize: 50.0,
+                              color: Colors.amber),
                         ),
-                      ),
-                      SpacingConsts().smallHeightBetweenFields(context),
-                      SizedBox(
-                        width: MediaQuery.of(context).size.width * 0.7,
-                        child: CustomTextField(
-                          controller: emailController,
-                          hintText: 'Enter email',
-                          prefixIcon: Icons.person,
+                        SpacingConsts().mediumHeightBetweenFields(context),
+                        const AutoSizeText(
+                          'Join the ranks!! Identify yourself!',
+                          style: TextStyle(
+                              fontFamily: 'Kod',
+                              fontSize: 20.0,
+                              color: Colors.amber),
                         ),
-                      ),
-                      SpacingConsts().smallHeightBetweenFields(context),
-                      CustomButton(
-                        buttonText: 'Submit',
-                        buttonHeight: 0.06,
-                        buttonWidth: 0.7,
-                        color: Colors.amber,
-                        onPressed: () {
-                          if (nameController.text.isEmpty ||
-                              emailController.text.isEmpty) {
-                            SnackBar snackBar = SnackBar(
-                                elevation: 0.8,
-                                behavior: SnackBarBehavior.floating,
-                                backgroundColor: Colors.amber,
-                                width: MediaQuery.of(context).size.width * 0.8,
-                                content: const Center(
-                                  child: AutoSizeText(
-                                    'Please fill all fields',
-                                    maxLines: 1,
-                                    style: TextStyle(fontSize: 18.0),
-                                  ),
-                                ));
-                            ScaffoldMessenger.of(context)
-                                .showSnackBar(snackBar);
-                          } else {
-                            context
-                                .read<MandatoryFieldsCubit>()
-                                .setMandatoryFields(
-                                    nameController.text, emailController.text);
-                          }
-                        },
-                      )
-                    ],
+                        SpacingConsts().mediumHeightBetweenFields(context),
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width * 0.7,
+                          child: CustomTextField(
+                            hintText: 'Enter name',
+                            controller: nameController,
+                            prefixIcon: Icons.person,
+                          ),
+                        ),
+                        SpacingConsts().smallHeightBetweenFields(context),
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width * 0.7,
+                          child: CustomTextField(
+                            controller: emailController,
+                            hintText: 'Enter email',
+                            prefixIcon: Icons.person,
+                          ),
+                        ),
+                        SpacingConsts().mediumHeightBetweenFields(context),
+                        CustomButton(
+                          buttonText: 'Submit',
+                          buttonHeight: 0.06,
+                          buttonWidth: 0.7,
+                          color: Colors.amber,
+                          onPressed: () {
+                            if (nameController.text.isEmpty ||
+                                emailController.text.isEmpty) {
+                              SnackBar snackBar = SnackBar(
+                                  elevation: 0.8,
+                                  behavior: SnackBarBehavior.floating,
+                                  backgroundColor: Colors.amber,
+                                  width:
+                                      MediaQuery.of(context).size.width * 0.8,
+                                  content: const Center(
+                                    child: AutoSizeText(
+                                      'Please fill all fields',
+                                      maxLines: 1,
+                                      style: TextStyle(fontSize: 18.0),
+                                    ),
+                                  ));
+                              ScaffoldMessenger.of(context)
+                                  .showSnackBar(snackBar);
+                            } else {
+                              context
+                                  .read<MandatoryFieldsCubit>()
+                                  .setMandatoryFields(nameController.text,
+                                      emailController.text);
+                            }
+                          },
+                        ),
+                        SpacingConsts().mediumHeightBetweenFields(context),
+                        CustomButton(
+                          buttonText: 'Exit',
+                          buttonHeight: 0.06,
+                          buttonWidth: 0.7,
+                          color: Colors.amber,
+                          onPressed: () {
+                            context.read<AuthCubit>().signOut();
+                          },
+                        )
+                      ],
+                    ),
                   ),
                 ),
               ),
